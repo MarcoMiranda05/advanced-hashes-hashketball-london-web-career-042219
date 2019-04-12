bb44bc5546b437d80c7905132dcc35400543ac0c
Marco Miranda <marmirand@hotmail.com>
2019-04-12 11:34:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,9 @@ def game_hash
   match = {:home => {
       :team_name => "Brooklin Nets",
       :colors => "Black, White",
-      :players => []
+      :players => [
+        {player_name: "Alan Anderson", number: "0", shoe: "16", points: "22", rebounds: "12", assists: "12", steals: "3", blocks: "1", slam_dunks: "1"}
+      ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
