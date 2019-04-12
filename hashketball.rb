126c6735530476c7e1ca9d44d11b2b6b64e4204e
Marco Miranda <marmirand@hotmail.com>
2019-04-12 11:29:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,14 @@
 def game_hash
-  match = {:home => {:team_name => "Brooklin Nets", :color => "Black, White", :players => []},:away => {}
+  match = {:home => {
+      :team_name => "Brooklin Nets",
+      :color => "Black, White",
+      :players => []
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :color => "Tuorquaise, Purple", 
+      :players => []
+    }
 
   }
 end
